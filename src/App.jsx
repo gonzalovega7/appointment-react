2b5d283314d17646dde44ca353af033ca9eb28e6
Gonzalo Vega <gonzalovega7@gmail.com>
2022-05-04 11:15:55 -0300

    Every time a new patient is inserted it is added to the patient list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import Form from "./components/Form";
 import Header from "./components/Header";
 import PatientList from "./components/PatientList";
 
 function App() {
   // JavaScript code
-
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
 
   return (
     <div className="container mx-auto mt-20">
-      <Header />
+      <Header/>
       <div className="mt-12 md:flex">
-        <Form />
-        <PatientList />
+        <Form 
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+        />
+        <PatientList 
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+        />
       </div>
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,80 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
+import Error from './Error';
 
-const Form = () => {
+const Form = ({ pacientes, setPacientes, paciente }) => {
+
+    const [nombre, setNombre] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+
+        console.log(paciente);
+    
+    },  [paciente]);
+    
+    const generarId = () => {
+
+        const random = Math.random().toString(36).substr(2);
+        const date = Date.now().toString(36);
+
+        return random + date;
+    }
+    
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        // Validacion del formulario
+        if([ nombre, propietario, email, fecha, sintomas ].includes('')) {
+            
+            setError(true);
+
+            return;
+
+        } 
+        
+        setError(false);
+
+        // Objeto de pacientes
+        const objetoPaciente = {
+            nombre,
+            propietario,
+            email,
+            fecha,
+            sintomas,
+            id: generarId()
+        }
+
+        setPacientes([...pacientes, objetoPaciente]);
+
+        // Reiniciar el form
+        setNombre('');
+        setPropietario('');
+        setEmail('');
+        setFecha('');
+        setSintomas('');
+
+    }
+
     return (
-        <div className="md:w-1/2 lg:w-2/5">
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
             <p className="text-lg mt-5 text-center mb-10">
                 Añade Pacientes y {' '} <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" 
+            >
+
+                {error && <Error mensaje='Todos los campos son obligatorios'/> }    
+
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                         Nombre Mascota
@@ -19,6 +84,8 @@ const Form = () => {
                         type="text" 
                         placeholder="Nombre de la mascota"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
                     />
                 </div>
 
@@ -31,6 +98,8 @@ const Form = () => {
                         type="text" 
                         placeholder="Nombre del propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                        onChange={(e) => setPropietario(e.target.value)}
                     />
                 </div>
 
@@ -44,6 +113,8 @@ const Form = () => {
                         type="email" 
                         placeholder="Ingrese su email de contacto"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
 
@@ -56,6 +127,8 @@ const Form = () => {
                         id="alta"
                         type="date" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)}
                     />
                 </div>
 
@@ -67,6 +140,8 @@ const Form = () => {
                         id="sintomas"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Describe los sintomas.."
+                        value={sintomas}
+                        onChange={(e) => setSintomas(e.target.value)}
                     />
                 </div>
 
